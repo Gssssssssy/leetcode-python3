20ee652c613461cd5a3af1133c78e3c4f3cc15dd
Gssssssssy <coder.gsy@gmail.com>
2018-01-23 10:16:34 +0800

    Accept.未进行性能优化

---
+++ q2_add_two_numbers.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            t_sum = (x + y + carry) % 10
+            carry = (x + y + carry) / 10
+            current.next = ListNode(t_sum)
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        if carry > 0:
+            current.next = ListNode(1)
+
+        return dummy.next
